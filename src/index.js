f6f8a6b3ed747a0dc9d542902cf643148edccbd2
vnln <bananiqus@gmail.com>
2022-01-24 10:20:21 +0400

    init: check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = check = (str, bracketsConfig) => {
+  const stack = [];
+  const pairs = {};
+  bracketsConfig.map(([a, b]) => (pairs[a] = b));
+  for (let bracket of str) {
+    if (pairs[bracket]) {
+      bracket == pairs[bracket] && bracket == stack[stack.length - 1] ? stack.pop() : stack.push(bracket);
+    } else if (Object.values(pairs).includes(bracket) && bracket !== pairs[stack.pop()]) {
+      return false;
+    }
+  }
+  return !stack.length;
+};
